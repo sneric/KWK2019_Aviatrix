a53b14ce6d484be354e248fccc145c73b7edb170
sneric <sneric.smith@gmail.com>
2019-06-18 21:49:59 -0700

    complete iteration 1

--- aviatrix_swift-master/AviatrixApp/Aviatrix.swift
+++ aviatrix_swift-master/AviatrixApp/Aviatrix.swift
@@ -11,6 +11,8 @@ import Foundation
 class Aviatrix {
     
     var author = ""
+    // add initial location so myPlane.location will work (iteration 1)
+    var location = "St. Louis"
     
     init(userName: String) {
         author = userName
@@ -18,7 +20,7 @@ class Aviatrix {
     var running = false
     
     func start() -> Bool {
-       // return true
+       // return true (iteration 0)
             running = true
             return running
         }
@@ -28,14 +30,20 @@ class Aviatrix {
     }
     
     func flyTo(destination : String) {
+        // one line to get new location to be your destination (iteration 1)
+       location = destination
         
     }
     
-    func distanceTo(target : String) {
-    
+    func distanceTo(current: String, target : String) -> Int{
+        // add -> Int to end of function! (interation 1)
+        // add the data variable to connect to AviatrixData.swift (iteration 1)
+        let data = AviatrixData()
+       // return data.knownDistances["St. Louis"]![target]! (iteration 1)
+        return data.knownDistances[current]![target]!
     }
     
     func knownDestinations() -> [String] {
-       return ["St. Louis"]
+       return ["St. Louis", "Phoenix", "Denver", "SLC"]
     }
 }
--- aviatrix_swift-master/AviatrixApp/main.swift
+++ aviatrix_swift-master/AviatrixApp/main.swift
@@ -12,7 +12,7 @@ func gauges(myPlane : Aviatrix) {
     print("Reading the gauges...")
     print(" ")
 //    print("| Running:  | ✅")
-//    print("| Location:  | \(myPlane.location)")
+    print("| Location:  | \(myPlane.location)")
 //    print("| Distance:  | \(myPlane.distanceTraveled) miles")
 //    print("| Fuel:      | \(myPlane.fuelLevel) gallons")
 //    print("| Max Fuel:  | \(myPlane.maxFuel) gallons")
@@ -26,7 +26,8 @@ func fly(myPlane : Aviatrix) {
     let destinations = myPlane.knownDestinations()
     
     for (index, city) in destinations.enumerated() {
-        let distance = myPlane.distanceTo(target: city)
+        // add current: myPlane.location to distance variable (iteration 1)
+        let distance = myPlane.distanceTo(current: myPlane.location, target: city)
         print("\(index): \(city), \(distance) miles")
     }
     
@@ -41,7 +42,7 @@ func fly(myPlane : Aviatrix) {
         
         if fuelCheck(myPlane: myPlane, destination : desiredLocation) {
             myPlane.flyTo(destination: desiredLocation)
-            print("🛬 You've arrived in _________!")
+            print("🛬 You've arrived in \(myPlane.location)!")
             gauges(myPlane: myPlane)
         }
     }
